b02a2ab7716a6675096dece0f30dc93fb632cfa8
BiBox.com <33807615+Biboxcom@users.noreply.github.com>
2020-12-17 20:57:35 +0800

    Update subUserAccountEvent.js

--- example/websocket/subUserAccountEvent.js
+++ example/websocket/subUserAccountEvent.js
@@ -6,14 +6,14 @@ const client = new BiboxFuturesClient( apiKey, secretKey );
 subscribeAccount = () => {
     client.subscribeAccount(  ( data ) => {
         console.log( data );
-        // [
-        //  {
+        // {
+        //  LTC:{
         //     asset: 'LTC',
         //     available: '0.198015884', // 可用
         //     orderMargin: '0',         // 挂单冻结
         //     positionMargin: '0'       // 仓位保证金
         //  },...
-        // ]
+        // }
     } );
 
 };
